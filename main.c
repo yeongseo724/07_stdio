696888102c119b57b8f481ce2f7bca687147adca
yeongseo724 <70522995+yeongseo724@users.noreply.github.com>
2020-10-15 16:19:08 +0900

    07 coding(4)

--- main.c
+++ main.c
@@ -3,27 +3,20 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-void sub(void);
+int counter;
+
+void set_counter()
+{
+	counter=20;
+}
 
 int main(int argc, char *argv[]) {
 	
-	int i;
-	for(i=0;i<3;i++)
-	{
-		sub();
-	}
+	printf("before function call i =%d\n", counter);
+	set_counter(counter);
+	printf("after function call i =%d\n", counter);
 	
 	return 0;
 }
 
-void sub(void)
-{
-	int auto_count=0;
-	static int static_count=0;
-	
-	auto_count++;
-	static_count++;
-	printf("auto_count=%d\n", auto_count);
-	printf("static_count=%d\n", static_count);
-	
-}
+
